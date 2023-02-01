15a5d2eeff8476f617b942e3b193b0726ec791fe
Raytopia <54505044+raytopianprojects@users.noreply.github.com>
2023-01-31 20:02:28 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from panda3d.core import GeoMipTerrain, DirectionalLight, PointLight, AmbientLight, Spotlight
+from panda3d.core import GeoMipTerrain, DirectionalLight, PointLight, AmbientLight, Spotlight, Vec2, Vec3, Vec4, Point2, \
+     Point3, Point4, ShaderTerrainMesh
 from direct.showbase.ShowBase import ShowBase
 from direct.task.Task import Task
 import traceback
 from math import cos, sin
 from direct.gui.DirectGui import DirectButton, DirectCheckButton, DirectRadioButton, DirectDialog, DirectEntry, DirectFrame, \
     DirectLabel, DirectOptionMenu, DirectScrolledList, DirectWaitBar, DirectSlider, DirectScrollBar, DirectScrolledFrame
+from panda3d.bullet import BulletWorld, BulletBoxShape, BulletCapsuleShape, BulletPlaneShape, BulletConeShape, BulletRigidBodyNode, \
+     BulletDebugNode, BulletSphereShape, BulletCylinderShape, BulletConvexHullShape, BulletTriangleMesh, BulletHeightfieldShape, \
+     BulletGhostNode, BulletHingeConstraint, BulletSliderConstraint, BulletSphericalConstraint, BulletConeTwistConstraint, \
+     BulletGenericConstraint, BulletVehicle, BulletVehicleTuning, BulletSoftBodyNode, BulletSoftBodyMaterial, \
+     BulletSoftBodyConfig
+from direct.particles.ParticleEffect import ParticleEffect
+import json
+from direct.interval.LerpInterval import LerpPosInterval
+
+class Light:
+    def __init__(self):
+        ...
+
+class Server:
+    def __init__(self):
+        ...
+
+class Client:
+    def __init__(self):
+        ...
+
 
 def main():        
     game = ShowBase()
@@ -54,7 +76,41 @@ def main():
                                    "WaitBar": DirectWaitBar,
                                    "Slider": DirectSlider,
                                    "ScrollBar": DirectScrollBar,
-                                   "ScrolledFrame": DirectScrolledFrame})
+                                   "ScrolledFrame": DirectScrolledFrame,
+                                   "Vec2": Vec2,
+                                   "Vec3": Vec3,
+                                   "Vec4": Vec4,
+                                   "Point2": Point2,
+                                   "Point3": Point3,
+                                   "Point4": Point4,
+                                   "BulletWorld": BulletWorld,
+                                   "BoxShape": BulletBoxShape,
+                                   "CapsuleShape": BulletCapsuleShape,
+                                   "PlaneShape": BulletPlaneShape,
+                                   "ConeShape": BulletConeShape,
+                                   "RigidBodyNode": BulletRigidBodyNode,
+                                   "BulletDebugNode": BulletDebugNode,
+                                   "SphereShape": BulletSphereShape,
+                                   "CylinderShape": BulletCylinderShape,
+                                   "ConvexHullShape": BulletConvexHullShape,
+                                   "TriangleMeshShape": BulletTriangleMesh,
+                                   "HeightfieldShape": BulletHeightfieldShape,
+                                   "GhostNode": BulletGhostNode,
+                                   "HingeConstraint": BulletHingeConstraint,
+                                   "SliderConstraint": BulletSliderConstraint,
+                                   "SphericalConstraint": BulletSphericalConstraint,
+                                   "ConeTwistConstraint": BulletConeTwistConstraint,
+                                   "GenericConstraint": BulletGenericConstraint,
+                                   "BulletVehicle": BulletVehicle,
+                                   "BulletVehicleTuning": BulletVehicleTuning,
+                                   "SoftBodyNode": BulletSoftBodyNode,
+                                   "SoftBodyMaterial": BulletSoftBodyMaterial,
+                                   "BulletSoftBodyConfig": BulletSoftBodyConfig,
+                                   "ParticleEffect": ParticleEffect,
+                                   "json": json,
+                                   "ShaderTerrainMesh": ShaderTerrainMesh,
+                                   "GeoMipTerrain": GeoMipTerrain,
+                                   "HeightfieldTesselator": HeightfieldTesselator})
                 
         except Exception as exception:
             traceback.print_exc()
